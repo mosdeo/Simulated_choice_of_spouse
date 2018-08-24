4f8b872b391516036e5a7830f65564e278c66e78
Lin Kao Yuan <mosdeo@gmail.com>
2018-08-25 05:25:22 +0800

    修正自覺分數次策略

--- Human.py
+++ Human.py
@@ -7,14 +7,24 @@ class Human(object):
         self.sex = sex
         self.real_score = real_score
         self.__feel_score = np.NaN
-        self.__feel_score_max = 0
-        self.__feel_score_min = 100
+        self.__feel_score_max = 100
+        self.__feel_score_min = 0
 
     def estimator_self_feel_score(self, isSelected, level_score):
         if(isSelected):
+            # 被選中，提高自信
             self.__feel_score_min = max(self.__feel_score_min, level_score)
         else:
+            # 沒被選中，降低自信
             self.__feel_score_max = min(self.__feel_score_max, level_score)
+            # 從最高分給自己打分數
+            self.__feel_score = self.__feel_score_max
+
+    def get_feel_score(self):
+        if(np.NaN == self.__feel_score):
+            return 100
+        else:
+            return self.__feel_score
 
     # 在清單中剃除同性
     # def __kick_same_sex(self, list_around_human):
