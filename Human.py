f49393f62db7bf4af0a2826c2a1195e02f6c3597
Lin Kao Yuan <mosdeo@gmail.com>
2018-08-25 08:47:47 +0800

    移入 def random_walk(x, y, input_matrix)

--- Human.py
+++ Human.py
@@ -26,3 +26,28 @@ class Human(object):
         else:
             return self.__feel_score
 
+    # 漫步移動
+    def random_walk(self, x, y, input_matrix):
+        # 處裡邊界條件
+        if(0==x):                          x_range = [x, x+1]
+        elif(x+1==len(input_matrix)):    x_range = [x-1, x]
+        else:                              x_range = [x-1, x, x+1]
+        if(0==y):                          y_range = [y, y+1]
+        elif(y+1==len(input_matrix[0])): y_range = [y-1, y]
+        else:                              y_range = [y-1, y, y+1]
+
+        # 對四周進行探訪
+        list_free_space = []
+        for i in x_range:
+            for j in y_range:
+                #這個位置沒人，放入候選
+                if(None == input_matrix[i][j]):
+                    list_free_space.append((i,j))
+
+        # 如果有找到空位
+        if(0 != len(list_free_space)):
+            next_x, next_y = list_free_space[np.random.randint(len(list_free_space))] #隨機選取沒人的位置
+            input_matrix[next_x][next_y] = input_matrix[x][y] #把自己放到新位置
+            input_matrix[next_x][next_y].wanted_x_y = None #經過移動之後要清除心儀對象
+            input_matrix[x][y] = None #舊位置空出來
+
--- make_pair.py
+++ make_pair.py
@@ -59,34 +59,7 @@ def find_self_feel_score(x, y, input_matrix):
             # 被選中，重新評估自覺分數
             else:
                 # print("被選中，重新評估自覺分數")
-                __input_matrix[x][y].estimator_self_feel_score(True, __input_matrix[i][j].real_score)
-
-# 漫步移動
-def random_walk(x, y, input_matrix):
-    # 處裡邊界條件
-    __input_matrix = input_matrix
-    if(0==x):                          x_range = [x, x+1]
-    elif(x+1==len(__input_matrix)):    x_range = [x-1, x]
-    else:                              x_range = [x-1, x, x+1]
-    if(0==y):                          y_range = [y, y+1]
-    elif(y+1==len(__input_matrix[0])): y_range = [y-1, y]
-    else:                              y_range = [y-1, y, y+1]
-
-    # 對四周進行探訪
-    list_free_space = []
-    for i in x_range:
-        for j in y_range:
-            #這個位置沒人，放入候選
-            if(None == __input_matrix[i][j]):
-                list_free_space.append((i,j))
-
-    # 如果有找到空位
-    if(0 != len(list_free_space)):
-        next_x, next_y = list_free_space[np.random.randint(len(list_free_space))] #隨機選取沒人的位置
-        __input_matrix[next_x][next_y] = __input_matrix[x][y] #把自己放到新位置
-        __input_matrix[next_x][next_y].wanted_x_y = None #經過移動之後要清除心儀對象
-        __input_matrix[x][y] = None #舊位置空出來
-        
+                __input_matrix[x][y].estimator_self_feel_score(True, __input_matrix[i][j].real_score)        
 
 # 展示分布
 def display_plane(input_matrix):
@@ -164,7 +137,7 @@ for T in range(1000):
     for i in range(len(list_2D_plane)):
         for j in range(len(list_2D_plane[i])):
             if(None == list_2D_plane[i][j]): continue #這個位置沒人
-            random_walk(i, j, list_2D_plane)
+            list_2D_plane[i][j].random_walk(i, j, list_2D_plane)
     display_plane(list_2D_plane)
     cv.waitKey(1)
 
